98247428eec05384d2dda6b80de87b690dcb6d89
Данил Гарбажий <danil@MacBook-Pro-Danil.local>
2020-09-16 22:33:22 +0300

    lab 1 2

--- src/main/java/ru/icmit/oodb/lab5/Lab5Main.java
+++
@@ -1,8 +0,0 @@
-package ru.icmit.oodb.lab5;
-
-public class Lab5Main {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/ru/icmit/oodb/lab6/graph/RelationType.java
+++
@@ -1,8 +0,0 @@
-package ru.icmit.oodb.lab6.graph;
-
-public enum RelationType {
-    OneToOne,
-    ManyToOne,
-    OneToMany,
-    ManyToMany;
-}
